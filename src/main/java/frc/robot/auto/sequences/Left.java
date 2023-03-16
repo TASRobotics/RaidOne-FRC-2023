d35ef1b741b1f68a31005199bf5cdc81ddd46b51
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2023-03-16 17:42:14 +0800

    left path

--- src/main/java/frc/robot/auto/sequences/Left.java
+++ src/main/java/frc/robot/auto/sequences/Left.java
@@ -3,7 +3,6 @@ package frc.robot.auto.sequences;
 import edu.wpi.first.math.trajectory.Trajectory;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.auto.actions.DrivePath;
-import frc.robot.auto.actions.autobaltest;
 import frc.robot.auto.actions.Action;
 import frc.robot.auto.actions.ActionGroup;
 
@@ -11,19 +10,19 @@ import java.util.Arrays;
 
 import com.pathplanner.lib.PathPlanner;
 
-public class leftFront extends AutoSequence {
-    private static final Trajectory path1 = PathPlanner.loadPath("forward", 1.5, AutoConstants.MAX_ACCEL);
+public class Left extends AutoSequence {
+    private static final Trajectory path1 = PathPlanner.loadPath("Left", 1.5, AutoConstants.MAX_ACCEL);
     private static final Trajectory path2 = PathPlanner.loadPath("Cone forward", 1.5, AutoConstants.MAX_ACCEL, true);
-    private static final autobaltest autobal = new autobaltest();
-    public leftFront() {}
+    //private static final autobaltest autobal = new autobaltest();
+    public Left() {}
 
     @Override
     public void sequence() {
         addAction(
             new ActionGroup(Arrays.asList(
-                new DrivePath(path2, true),
-                new DrivePath(path1),
-                autobal
+                new DrivePath(path1, true)
+                //new DrivePath(path1),
+                //autobal
             )
         ));
     }
