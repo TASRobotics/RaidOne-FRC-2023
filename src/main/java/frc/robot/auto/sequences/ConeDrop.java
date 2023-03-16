e649f3dc50723e538948a3fe6ff01445fc622a88
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-03-16 16:22:20 +0800

    Finished middle path
    Cone drop included, autobal is testing

--- src/main/java/frc/robot/auto/sequences/ConeDrop.java
+++ src/main/java/frc/robot/auto/sequences/ConeDrop.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import com.pathplanner.lib.PathPlanner;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import frc.robot.Constants.AutoConstants;
 import frc.robot.auto.actions.Action;
 import frc.robot.auto.actions.ActionGroup;
 import frc.robot.auto.actions.DrivePath;
@@ -11,17 +15,17 @@ import frc.robot.auto.actions.RamBack;
 
 public class ConeDrop extends AutoSequence {
 
-    public ConeDrop() {
+    private static final String name = "CrossPlatform";
 
-    }
+    private static final Trajectory path1 = PathPlanner.loadPath(name, AutoConstants.MAX_VEL, AutoConstants.MAX_ACCEL);
+
+    public ConeDrop() {}
 
     @Override
     public void sequence() {
         addAction(
             new ActionGroup(Arrays.asList(
-                new RamBack()
-                )
-                //new DrivePath(path1, true))
+                new DrivePath(path1, true))
             )
         );
         
--- src/main/java/frc/robot/auto/sequences/leftFront.java
+++ src/main/java/frc/robot/auto/sequences/leftFront.java
@@ -3,6 +3,8 @@ package frc.robot.auto.sequences;
 import edu.wpi.first.math.trajectory.Trajectory;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.auto.actions.DrivePath;
+import frc.robot.auto.actions.autobaltest;
+import frc.robot.auto.actions.Action;
 import frc.robot.auto.actions.ActionGroup;
 
 import java.util.Arrays;
@@ -10,14 +12,18 @@ import java.util.Arrays;
 import com.pathplanner.lib.PathPlanner;
 
 public class leftFront extends AutoSequence {
-    private static final Trajectory path1 = PathPlanner.loadPath("Left", 1.5, AutoConstants.MAX_ACCEL);
+    private static final Trajectory path1 = PathPlanner.loadPath("forward", 1.5, AutoConstants.MAX_ACCEL);
+    private static final Trajectory path2 = PathPlanner.loadPath("Cone forward", 1.5, AutoConstants.MAX_ACCEL, true);
+    private static final autobaltest autobal = new autobaltest();
     public leftFront() {}
 
     @Override
     public void sequence() {
         addAction(
             new ActionGroup(Arrays.asList(
-                new DrivePath(path1, true)
+                new DrivePath(path2, true),
+                new DrivePath(path1),
+                autobal
             )
         ));
     }
